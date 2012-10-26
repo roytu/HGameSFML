c7ab3aa5fd81fa45a10fac73686955bbaef774e6
Roy Tu <thatsnail@gmail.com>
2012-10-26 14:58:01 -0400

    LDraw DrawSprite offset fix

--- src/LDraw.cpp
+++ src/LDraw.cpp
@@ -20,8 +20,8 @@ void LDraw::DrawImage(sf::Image* img, double x, double y)
 void LDraw::DrawSprite(Spr* spr, double imageIndex, double x, double y)
 {
     sf::Sprite s(*spr->image);
-    s.SetX(x);
-    s.SetY(y);
+    s.SetX(x - spr->origX);
+    s.SetY(y - spr->origY);
     s.SetSubRect(sf::IntRect(imageIndex * spr->imageWidth, 0, (imageIndex + 1) * spr->imageWidth, spr->imageHeight));
     HScreen::getScreen()->Draw(s);
 }
